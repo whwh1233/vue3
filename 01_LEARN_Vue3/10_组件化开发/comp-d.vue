1624c01a0f04afa25d7048e093e4ab0a70adf41d
whwh1233 <1249156074@qq.com>
2021-08-29 17:08:15 +0800

    05 finished in lab

---
+++ 01_LEARN_Vue3/10_组件化开发/comp-d.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>{{title}}</h2>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title:'我是comp-a的标题'
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
